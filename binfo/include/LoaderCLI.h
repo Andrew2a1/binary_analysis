5f21a72d4c3bb588e90678e09c19aec8bec13d5e
Andrew2a1 <adam.brz@onet.eu>
2022-10-31 23:00:35 +0100

    Add elfinject example code

--- binfo/include/LoaderCLI.h
+++ binfo/include/LoaderCLI.h
@@ -12,7 +12,8 @@ private:
     Binary bin;
 
 public:
-    LoaderCLI(const std::string &binary_name);
+    explicit LoaderCLI(const std::string &binary_name);
+
     void show_sections() const;
     void show_section_data(const std::string &section_name, int data_bytes_per_row = 16) const;
     void show_symbols(bool demangle = false) const;
